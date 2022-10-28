8437ee19ec8d1772c8a1a24d13d6579a888d23a0
hamzat <lawalhamzat27@gmail.com>
2022-10-28 18:12:22 +0100

    fixed cors

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors({
+    origin: '*'
+}));
 app.get('/',(req,res)=>{
     res.status(200).send({ "slackUsername": "lawalH", "backend": true, "age": 21, "bio":"a fullstack dev" })
 })
